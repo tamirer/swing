83493b88a2a6fbb7d8ee7441becafc11b494be44
tamirer <tamirer@post.bgu.ac.il>
2017-05-23 17:51:15 +0300

    s

--- src/swingTest/Ramram.java
+++ src/swingTest/Ramram.java
@@ -1,8 +1,14 @@
 package swingTest;
 
+import java.awt.Point;
+
 public class Ramram extends MovableImage {
 
 	public Ramram() {
 		super("ramram.jpg");
+	}
+	
+	public Ramram(Point p){
+		super("ramram.jpg",p);
 	}
 }
